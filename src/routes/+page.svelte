ec3d0c5515a1fa1fb94ee00d20c264d6a28bf1d1
bauerbrun0 <bauerbruno@protonmail.com>
2024-03-03 12:22:16 +0100

    feat: add skeleton ui and tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button type="button" class="btn variant-filled">Button</button>
+
+<div class="card p-4 text-primary-500">Basic</div>
