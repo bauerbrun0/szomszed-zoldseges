2cf515c28641c8b7e6c56469a72330ea960958a1
bauerbrun0 <bauerbruno@protonmail.com>
2024-03-05 22:05:06 +0100

    feat: add content to home page for unauthed users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
+<script lang="ts">
+	import { vegetables, fruits } from "$lib/data";
+	import ProductTable from "./components/ProductTable.svelte";
+
+	$: userLoggedIn = false;
+</script>
+
 <div class="
 	mx-auto 2xl:max-w-screen-2xl 2xl:min-w-screen-2xl
 	px-2 md:px-4
 ">
-	Home Page
+	{#if userLoggedIn}
+		<h3 class="h3 my-6">Raktárkészlet</h3>
+		<h3 class="h3 my-6">Szupertitkos Raktárkészlet</h3>
+		<h3 class="h3 my-6">Beszállítók</h3>
+		<h3 class="h3 my-6">Szupertitkos Beszállítók</h3>
+	{:else}
+		<ProductTable products={vegetables} title="Zöldségeink" />
+		<ProductTable products={fruits} title="Gyümölcseink" />
+	{/if}
 </div>
---
+++ src/routes/components/ProductTable.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Product } from "$lib/types";
+
+	export let products: Product[];
+	export let title: string;  	
+</script>
+
+<h3 class="h3 my-4">{title}</h3>
+<div class="table-container mb-8">
+	<table class="table table-hover table-compact">
+		<thead class="text-left">
+			<th>Termék</th>
+			<th>Származási hely</th>
+			<th>Készlet</th>
+			<th>Ár</th>
+		</thead>
+		<tbody>
+			{#each products as product}
+				<tr class="h-px">
+					<td>
+						<div class="flex h-full space-x-2 md:space-x-4">
+							<img src={product.imagePath} alt={product.name} class="w-20 h-20 object-cover rounded-lg" />
+							<span>{product.name}</span>
+						</div>
+					</td>
+					<td>{product.origin}</td>
+					<td>{product.stock}</td>
+					<td>{product.price}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
