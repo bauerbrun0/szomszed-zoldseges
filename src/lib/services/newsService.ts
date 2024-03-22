a3b6b4f3f975584851dc4e3522765a4d438c1056
bauerbrun0 <bauerbruno@protonmail.com>
2024-03-22 15:46:05 +0100

    feat: log news creation

--- src/lib/services/newsService.ts
+++ src/lib/services/newsService.ts
@@ -1,6 +1,7 @@
 import config from "$lib/configs/app.config";
 import db from "$lib/db";
 import { news, type News } from "$lib/db/schema";
+import logger from "$lib/utils/logger";
 import { eq } from "drizzle-orm";
 import { mdToPdf } from "md-to-pdf";
 
@@ -11,6 +12,8 @@ async function createNews(name: string, content: string): Promise<void> {
 	// Remove comments from the markdown content, keep ---js comments for md-to-pdf
 	const markdownForPdf = content.replace(commentRegexWithJs, "");
 	const contentToStore = content.replace(commentRegex, "");
+
+	logger.info("Creating news", { name, content, markdownForPdf, contentToStore });
 
 	const [res] = await db
 		.insert(news)
@@ -26,6 +29,7 @@ async function createNews(name: string, content: string): Promise<void> {
 			dest: `${config.NEWS_PDF_DIR}/${res.id}.pdf`,
 			...config.MD_TO_PDF_OPTIONS
 		});
+		logger.info("News created", { id: res.id, name, content, markdownForPdf, contentToStore });
 	} catch (error) {
 		// can't get to work tx.rollback(), so there's go transaction
 		await db.delete(news).where(eq(news.id, res.id));
