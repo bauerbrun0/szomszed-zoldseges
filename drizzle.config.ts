4d788003cc5e8c21a8e8541ddc6529b9cc204133
bauerbrun0 <bauerbruno@protonmail.com>
2024-03-22 12:16:08 +0100

    feat: add news inserts to setup.ts refactor: add app config using dotenv package refactor: rename drizzle directory to migrations refactor: restructure db and pdfs/news directories build: modify package.json scripts fix: move favicon <link/> to +layout.svelte

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	schema: "./src/lib/db/schema.ts",
-	out: "./drizzle",
+	out: "./migrations",
 	driver: "better-sqlite",
 	dbCredentials: {
-		url: "./drizzle/.db/sqlite.db"
+		url: "./data/db/sqlite.db",
 	},
 	verbose: true,
 	strict: true
